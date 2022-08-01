556b63fd66c002f9ec3d8c322cf80b8c22009666
Deni Gunawan <artdengun@gmail.com>
2022-08-01 13:42:00 +0700

    update

---
+++ src/main/java/com/deni/gunawan/backendspringangular/BackendSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.deni.gunawan.backendspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringAngularApplication.class, args);
+	}
+
+}
